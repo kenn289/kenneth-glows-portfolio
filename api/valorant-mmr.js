dce8ee132d5692c2549fdb971de8946946c9440b
Kenneth Oswin <kennethoswin289@gmail.com>
2025-10-01 19:21:52 +0530

    Updated api

--- api/valorant-mmr.js
+++ api/valorant-mmr.js
@@ -2,6 +2,11 @@ export default async function handler(req, res) {
     const { region, name, tag } = req.query;
     const apiKey = process.env.HENRIK_API_KEY;
   
+    // DEBUG ONLY: check if env is read
+    if (!apiKey) {
+      return res.status(500).json({ error: "Env var HENRIK_API_KEY is missing!" });
+    }
+  
     try {
       const url = `https://api.henrikdev.xyz/valorant/v2/mmr/${region}/${name}/${tag}`;
       const response = await fetch(url, {
